71a7065315ea835ba7b1ecb9bcd03f10bb955b27
SravaniSomalaraju <somalarajusravani8@gmail.com>
2025-12-25 17:33:03 +0530

    uc2[sravani]:encapsulation using setters and getters

--- src/vehicle.py
+++ src/vehicle.py
@@ -3,3 +3,30 @@ class Vehicle:
         self.vehicle_id = vehicle_id
         self.model = model
         self.battery_percentage = battery_percentage
+
+        self.__maintenance_status = "Available"         #uc1
+        self.__rental_price = 0.0
+
+    def set_battery_percentage(self, battery):
+        if battery >= 0 and battery <= 100:
+            self.battery_percentage = battery
+        else:
+            print("Battery percentage must be between 0 and 100")
+
+    def get_battery_percentage(self):
+        return self.battery_percentage
+    
+    def set_maintenance_status(self, status):
+        self.__maintenance_status = status
+
+    def get_maintenance_status(self):
+        return self.__maintenance_status
+
+    def set_rental_price(self, price):
+        if price >= 0:
+            self.__rental_price = price
+        else:
+            print("Rental price cannot be negative")
+
+    def get_rental_price(self):
+        return self.__rental_price
