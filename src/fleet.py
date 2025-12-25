e2075423c333437952088e105b40d820b462b9b0
SravaniSomalaraju <somalarajusravani8@gmail.com>
2025-12-25 17:57:10 +0530

    uc6[sravani]:multiple hubs using dictionary

---
+++ src/fleet.py
@@ -0,0 +1,22 @@
+class FleetManager:
+    def __init__(self):
+        # Dictionary to store hubs and vehicles
+        self.hubs = {}
+
+    def add_hub(self, hub_name):
+        if hub_name not in self.hubs:
+            self.hubs[hub_name] = []
+            print(f"Hub '{hub_name}' added successfully")
+        else:
+            print(f"Hub '{hub_name}' already exists")
+
+    def add_vehicle_to_hub(self, hub_name, vehicle):
+        if hub_name in self.hubs:
+            self.hubs[hub_name].append(vehicle)
+            print(f"Vehicle added to hub '{hub_name}'")
+        else:
+            print(f"Hub '{hub_name}' does not exist")
+
+    def show_hubs(self):
+        for hub, vehicles in self.hubs.items():
+            print(hub, "has", len(vehicles), "vehicles")
